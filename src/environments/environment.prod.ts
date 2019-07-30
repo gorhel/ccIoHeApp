6b7aa00e1ff063a1a9bb66970a28ab5c40bb63c0
essy kouame <essy.kouame@gmail.com>
2019-07-30 11:13:00 +0400

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAI_LI2nwvP1IzvmT3jZTP0AV_6UqK-yro",
+    authDomain: "centreculturel-d1115.firebaseapp.com",
+    databaseURL: "https://centreculturel-d1115.firebaseio.com",
+    projectId: "centreculturel-d1115",
+    storageBucket: "",
+    messagingSenderId: "216990502082",
+    appId: "1:216990502082:web:1d27e0aa5af5cf90"
+  }
 };
